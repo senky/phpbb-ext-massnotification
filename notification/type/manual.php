e43b097a2ae4bb25372f40a0e27b32afd5490bf0
Paul Norman <31554246+CrazyC0d3r@users.noreply.github.com>
2019-01-12 13:45:19 +0000

    Fixed insecure redirect error
    phpBB displays the following error when adding a redirect url to the notification and a user first clicks on the link in the notification:
    
    General Error
    Tried to redirect to potentially insecure url.
    
    Adding the public function get_redirect_url() fixes this issue.

--- notification/type/manual.php
+++ notification/type/manual.php
@@ -123,6 +123,14 @@ class manual extends \phpbb\notification\type\base
 	}
 
 	/**
+	* {inheritDoc}
+	*/
+	public function get_redirect_url()
+	{
+		return false;
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	public function get_email_template()
