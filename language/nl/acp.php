608336311195e669e86b046081f2d83f714d94a7
Andy Dm <demarieandy@outlook.com>
2019-03-07 23:49:31 +0100

    NL 1.0.2

--- language/nl/acp.php
+++ language/nl/acp.php
@@ -19,7 +19,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'MASSNOTIFICATION_DESC'	=> 'Here you can send a board notification to either all of your users or specific users or groups.',
+	'MASSNOTIFICATION_DESC'	=> 'Hier kunt u een forum notificatie sturen naar al uw gebruikers of specifieke gebruikers of groepen.',
 
 	'COMPOSE'				=> 'Maak massa notificatie',
 	'ALL_USERS'				=> 'Alle gebruikers',
@@ -28,14 +28,14 @@ $lang = array_merge($lang, array(
 	'SEND_TO_USERS_EXPLAIN'	=> 'Het invoeren van namen hier overschrijft elke groep die hierboven is geselecteerd. Voer elke gebruikersnaam op een nieuwe regel in.',
 	'TITLE'					=> 'Notificatie titel',
 	'MESSAGE_EXPLAIN'		=> 'Houd het kort, bij voorkeur onder 150 karakters. Je kan HTML gebruiken.',
-	'URL'					=> 'Omleidings-URL',
+	'URL'					=> 'Omleiding URL',
 	'URL_EXPLAIN'			=> 'Voer de URL in waarnaar de gebruiker wordt omgeleid nadat hij op de melding heeft geklikt.',
-	'AUTHOR_EXPLAIN'		=> 'Enter author of the notification. Leave blank for generic notification without author.',
+	'AUTHOR_EXPLAIN'		=> 'Voer de auteur van de melding in. Leeg laten voor generieke melding zonder auteur.',
 	'SEND_NOTIFICATION'		=> 'Verzend notificatie',
 
 	'NO_NOTIFICATION_TITLE'		=> 'U moet de titel van de notificatie opgeven.',
 	'NO_NOTIFICATION_MESSAGE'	=> 'U moet een notificatie bericht opgeven.',
-	'NO_NOTIFICATION_AUTHOR'	=> 'Author does not exist.',
+	'NO_NOTIFICATION_AUTHOR'	=> 'Auteur bestaat niet.',
 
 	'NOTIFICATION_SEND'	=> 'Uw notificatie is verzonden.',
 ));
