8f14dac9c49b66fefb065ad33e1962501c2b4173
eunaumtenhoid <eunaumtenhoid@outlook.com>
2019-03-10 11:25:58 -0300

    update

--- language/pt_br/info_acp_demo.php
+++ language/pt_br/info_acp_demo.php
@@ -5,7 +5,7 @@
  *
  * @copyright (c) 2019, Jakub Senko
  * @license GNU General Public License, version 2 (GPL-2.0)
- * Brazilian Portuguese translation by eunaumtenhoid (c) 2019 [ver 1.0.0] (https://github.com/phpBBTraducoes)
+ * Brazilian Portuguese translation by eunaumtenhoid [2019][ver 1.0.2] (https://github.com/phpBBTraducoes)
  */
 
 if (!defined('IN_PHPBB'))
